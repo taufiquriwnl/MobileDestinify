03349eed0566ec735c42d98b7b9c18b03bb01b8f
unknown <fidargamer123@gmail.com>
2023-12-18 17:33:15 +0700

    Second Patch

--- app/src/main/java/com/example/tour_guide/MainActivity.kt
+++ app/src/main/java/com/example/tour_guide/MainActivity.kt
@@ -1,26 +1,21 @@
 package com.example.tour_guide
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.btn_1)
-        val button1: Button = findViewById(R.id.btn_2)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.fragment)
 
-        button.setOnClickListener {
-            val intent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
-        button1.setOnClickListener {
-            val intent = Intent(this@MainActivity, RegisterActivity::class.java)
-            startActivity(intent)
+        bottomNavigationView.setupWithNavController(navController)
         }
     }
-}
+
